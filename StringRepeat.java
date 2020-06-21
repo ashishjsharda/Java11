869ae0fe4dc13844bbfd2237f763bb4d9671b63d
Ashish S <ashishjsharda@gmail.com>
2020-06-21 15:26:04 -0400

    Create StringRepeat.java

---
+++ StringRepeat.java
@@ -0,0 +1,11 @@
+/**
+ * Using String Repeat
+ * @author ashish
+ */
+public class StringRepeat {
+    public static void main(String[] args) {
+        String str="Hello ";
+        System.out.println(str.repeat(3));
+
+    }
+}
