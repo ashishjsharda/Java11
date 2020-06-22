db236ffa4f011da2035ce09daea83cc38f531549
Ashish S <ashishjsharda@gmail.com>
2020-06-22 07:45:50 -0400

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Stack in Java
+ * @author ashish
+ */
+public class Stack {
+
+    List<Integer> list=new ArrayList<Integer>();
+
+    /**
+     * Push an item into stack
+     * @param num
+     */
+    public void push(int num)
+    {
+        list.add(num);
+    }
+
+    /**
+     * Pop an item from stack
+     * @return int
+     */
+    public int pop()
+    {
+        if(list==null)
+        {
+            System.out.println("Stack is empty");
+            System.exit(0);
+        }
+        return list.remove(list.size()-1);
+    }
+    public static void main(String[] args) {
+
+        Stack stack=new Stack();
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack.pop());
+    }
+}
