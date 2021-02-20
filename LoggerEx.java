e1ef42d3dc5cae6c5cb2c72d6fe075983cf1086f
Ashish S <ashishjsharda@gmail.com>
2021-02-20 06:57:49 -0500

    Create LoggerEx.java
    Example using Logger

---
+++ LoggerEx.java
@@ -0,0 +1,14 @@
+import java.util.logging.Logger;
+
+/**
+ * Example using Logger
+ *
+ * @author ashish
+ */
+public class LoggerEx {
+    public static void main(String[] args) {
+
+        Logger.getGlobal().info("Using Logger");
+        Logger.getGlobal().warning("Setting warning ");
+    }
+}
